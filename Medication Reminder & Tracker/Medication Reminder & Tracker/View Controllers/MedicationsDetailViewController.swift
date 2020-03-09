3bd5471994ca95fd4719b5c24b2ca15865895588
Dimitri Bouniol <dimitri.bouniol@lambda.com>
2020-03-09 00:07:26 -0400

    saving changes

--- Medication Reminder & Tracker/Medication Reminder & Tracker/View Controllers/MedicationsDetailViewController.swift
+++ Medication Reminder & Tracker/Medication Reminder & Tracker/View Controllers/MedicationsDetailViewController.swift
@@ -174,16 +174,3 @@ extension MedicationsDetailViewController: UITableViewDataSource, UITableViewDel
     }
 }
 
-extension UIViewController {
-    func setupToHideKeyboardOnTapOnView() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
-            target: self,
-            action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    @objc func dismissKeyboard()
-    {
-        view.endEditing(true)
-    }
-}
